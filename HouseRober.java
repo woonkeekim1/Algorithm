b88264093d4b29ba61afe462bf2ecc6cd9fa275f
woonkeekim1 <woonkeekim@gmail.com>
2017-10-22 21:48:11 -0700

    198 house robber

---
+++ HouseRober.java
@@ -0,0 +1,22 @@
+class HouseRober {
+  /*
+  * Given a list of non-negative integers representing the amount of money of
+  * each house, determine the maximum amount of money you can rob tonight
+  * without alerting the police.
+  * input: integer array
+  * output: maximum amount of money
+  */
+  public int rob(int[] nums) {
+    if(nums == null || nums.length == 0) {
+        return 0;
+    }
+
+    int size = nums.length;
+    int[] dp = new int[size];
+    dp[0] = nums[0];
+    for(int houseIndex = 1; houseIndex < size; houseIndex++) {
+        dp[houseIndex] = Math.max(dp[houseIndex-1], nums[houseIndex] + (houseIndex < 2 ? 0 : dp[houseIndex-2]));
+    }
+    return dp[size - 1];
+  }
+}
